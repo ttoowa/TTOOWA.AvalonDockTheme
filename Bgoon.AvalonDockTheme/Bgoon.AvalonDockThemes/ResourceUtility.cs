fe65092f6f24cba812bab0d7aca38911c148956f
Bgoon <isg1153@naver.com>
2020-05-31 20:01:38 +0900

    -Theme 구성요소 및 스크립트 추가

---
+++ Bgoon.AvalonDockTheme/Bgoon.AvalonDockThemes/ResourceUtility.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bgoon.AvalonDockTheme {
+	internal static class ResourceUtility {
+		public static Uri GetUri(string assemblyName, string path) {
+			return new Uri($"pack://application:,,,/{assemblyName};component/{path}", UriKind.Absolute);
+		}
+	}
+}
---
+++ Bgoon.AvalonDockTheme/Bgoon.AvalonDockThemes/Styles/FlatDark/FlatDarkTheme.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using AvalonDock.Themes;
+
+namespace Bgoon.AvalonDockTheme {
+	public class FlatDarkTheme : Theme {
+
+		public override Uri GetResourceUri() {
+				return ResourceUtility.GetUri("Bgoon.AvalonDockThemes", "Styles/FlatDark/FlatDark.xaml");
+		}
+	}
+}
