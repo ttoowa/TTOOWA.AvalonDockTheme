d188ac8dc3148186ac1124d8d89e634764fac1a6
TTOOWA <isg1153@naver.com>
2024-09-10 13:22:23 +0900

    - 자동 서식

--- TTOOWA.AvalonDockThemes/ResourceUtility.cs
+++ TTOOWA.AvalonDockThemes/ResourceUtility.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TTOOWA.AvalonDockTheme {
-	internal static class ResourceUtility {
-		public static Uri GetUri(string assemblyName, string path) {
-			return new Uri($"pack://application:,,,/{assemblyName};component/{path}", UriKind.Absolute);
-		}
-	}
+namespace TTOOWA.AvalonDockTheme;
+
+internal static class ResourceUtility {
+    public static Uri GetUri(string assemblyName, string path) {
+        return new Uri($"pack://application:,,,/{assemblyName};component/{path}", UriKind.Absolute);
+    }
 }
--- TTOOWA.AvalonDockThemes/Styles/FlatDark/FlatDarkTheme.cs
+++ TTOOWA.AvalonDockThemes/Styles/FlatDark/FlatDarkTheme.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using AvalonDock.Themes;
 
-namespace TTOOWA.AvalonDockTheme {
-	public class FlatDarkTheme : Theme {
+namespace TTOOWA.AvalonDockTheme;
 
-		public override Uri GetResourceUri() {
-			return ResourceUtility.GetUri("TTOOWA.AvalonDockThemes", "Styles/FlatDark/FlatDark.xaml");
-		}
-	}
+public class FlatDarkTheme : Theme {
+    public override Uri GetResourceUri() {
+        return ResourceUtility.GetUri("TTOOWA.AvalonDockThemes", "Styles/FlatDark/FlatDark.xaml");
+    }
 }
